05e3a21d8fde3ad44cb3b8282d1fcdb279e78695
Jesse Bryan <winneonsword@gmail.com>
2014-03-15 17:28:40 -0500

    Exprimental version 1.2

--- common/run.py
+++ common/run.py
@@ -1,11 +1,6 @@
-###################
-# LinkFixerBotSnr #
-# # #  By WS  # # #
-###################
+import sys, os, re, time, argparse, getpass, praw, requests, reddit, config
 
-import sys, os, re, time, argparse, getpass, praw, reddit, config
-
-default = {
+current = {
 
 	'credentials': {
 	
@@ -15,6 +10,7 @@ default = {
 	},
 	
 	'limit': '',
+	'banned': [],
 	
 }
 
@@ -23,17 +19,7 @@ sub_regex = re.compile(' r/[A-Za-z0-9_]+')
 
 def bootup():
 	
-	version = "1.1"
-	
-	banned = [
-		'loans', 'nba', 'aww', 'subredditdrama', 'againstmensrights', 'australia', 'shitpoliticssays',
-		'scotch', 'metacanada', 'news', 'nfl', 'breakingbad', 'theredpill', 'whatisthisthing', 'conspiratard',
-		'comics', 'mls', 'twoxchromosomes', 'politics', 'badhistory', '49ers', 'yugioh', 'mac', 'science',
-		'polticaldiscussion', 'photography,', 'srsgaming', 'europe', 'cringe', 'soccer', 'tatoos', 'wtf',
-		'buildapc', 'funny', 'nascar', 'conspiracy', 'gaming', 'fitness', 'iama', 'mensrights', 'adviceanimals',
-		'nottheonion', 'pics', 'okcupid', 'squaredcircle', 'makeupaddiction', 'trees', 'reactiongifs', 'india',
-		'askreddit', 'screenshots', 'askscience', 'sex'
-	]
+	version = "1.2"
 	
 	parse = argparse.ArgumentParser(description = 'LinkFixerBot')
 	parse.add_argument('-l', '--login', action = 'store_true', help = 'Login to a different account than config account')
@@ -44,7 +30,7 @@ def bootup():
 	
 	if not os.path.isfile(config_name):
 		
-		config.write(default, config_name)
+		config.write(current, config_name)
 		print('> Created config.json. Please edit the values in the config before continuing.')
 		sys.exit()
 		
@@ -72,16 +58,23 @@ def bootup():
 		user = conf['credentials']['username']
 		passwd = conf['credentials']['password']
 		
+		current['credentials']['username'] = user
+		current['credentials']['password'] = passwd
+		
+		current['limit'] = conf['limit']
+		
 	agent = (
-		'/u/' + user + ' running LinkFixerBot, version ' + version
+		'/u/' + user + ' running LinkFixerBot, version ' + version + ', created by /u/WinneonSword.'
 	)
+	
+	banned = []
 	
 	r = praw.Reddit(user_agent = agent)
 	reddit.login(user, passwd, r)
 	
-	loop(r, conf, banned)
+	loop(user, r, conf, banned)
 	
-def loop(reddit, config, banned):
+def loop(user, reddit, conf, banned):
 	
 	cache = []
 	
@@ -92,22 +85,82 @@ def loop(reddit, config, banned):
 		
 		while True:
 			
-			comments = reddit.get_comments('all', limit = config['limit'])
+			inbox = None
+			inbox = reddit.get_unread(limit = None)
+			
+			for message in inbox:
+				
+				try:
+					
+					if "+delete " in message.body:
+						
+						print('\n> Found potentially valid comment to be removed.')
+						
+						id = message.body.replace('+delete ', '')
+						comment = reddit.get_info(thing_id = 't1_' + id)
+						parent = reddit.get_info(thing_id = comment.parent_id)
+						
+						if comment.author.name == user and parent.author.name == message.author.name:
+							
+							print('> Comment is fully valid. Removing...')
+							comment.delete()
+							
+							print('\n> Comment removed!')
+							
+						else:
+							
+							print('> Comment is not valid!')
+							
+				except:
+					
+					print('> Failed to remove comment.')
+					
+				message.mark_as_read()
+				
+			comments = reddit.get_comments('all', limit = current['limit'])
 			
 			for comment in comments:
 				
 				cond, links = check(comment)
+				subreddit = comment.subreddit.display_name
 				
-				if cond and comment.subreddit.display_name.lower() not in banned and comment.id not in cache:
+				if cond and subreddit.lower() not in banned and comment.id not in cache:
 					
-					print('\n> Valid comment found in the sub /r/%s!' % comment.subreddit.display_name)
-					cache = post(comment, links, cache)
+					print('\n> Valid comment found in the sub /r/%s!' % subreddit)
 					
+					'''if current['credentials']['username'] not in reddit.get_banned(subreddit):
+						
+						print("\n> The user is banned from the comment's subreddit!")
+						print('> Adding subreddit to the banned subs list.')
+						
+						banned.append(subreddit.lower())
+						
+					else:
+						
+						cache = post(comment, links, cache)'''
+						
+					cache = post(reddit, comment, links, cache)
+						
 			time.sleep(15)
 			
+	except requests.exceptions.HTTPError:
+		
+		print('\n> An HTTP Error has occured. Restarting the bot.')
+		bootup()
+		
 	except KeyboardInterrupt:
 		
-		print('\n> Stopped LFB. Thank you for running this bot!')
+		print('\n> Stopping LFB. Writing the banned subs list to the config.')
+		
+		current['banned'] = banned
+		config.write(current, config_name)
+		
+		print('> Stopped LFB. Thank you for running this bot!')
+		
+	'''except:
+		
+		print('\n> An error has occured. Restarting the bot.')
+		loop(reddit, config, banned)'''
 		
 def check(comment):
 	
@@ -121,39 +174,52 @@ def check(comment):
 		
 	return cond, links
 	
-def post(comment, links, cache):
+def post(reddit, comment, links, cache):
 	
 	denied_links = ''
 	fixed = ''
 	
 	for c in links:
-	
-		text = '/' + c[1:] + ' '
 		
-		if c[1:].lower() == 'r/' + comment.subreddit.display_name.lower():
+		sub = c[1:]
+		text = '/' + sub + ' '
+		
+		if sub.lower() == 'r/' + comment.subreddit.display_name.lower():
 		
 			denied_links += text
 		
 		else:
-			
-			fixed += text
-			
+		
+			try:
+				
+				check = sub.replace('r/', '')
+				reddit.get_subreddit(check).get_hot(limit = 1).next()
+				
+				fixed += text
+				
+			except:
+				
+				denied_links += text
+				
 	if fixed == '':
 		
-		print('> All broken links are the same as the sub!')
+		print('> All broken links are the same as the sub or they are invalid links!')
 		
 	else:
 		
-		reply = (
-			fixed + '\n\n'
-			'*****\n'
-			'^This ^is ^an [^automated ^bot](http://github.com/WinneonSword/LFB)^. ^For ^reporting ^**problems**, ^contact ^/u/WinneonSword.'
-		)
-		
 		try:
 			
-			comment.reply(reply)
+			replied = comment.reply('This will be edited momentarily.')
 			cache.append(comment.id)
+			
+			reply = (
+				fixed + '\n\n'
+				'*****\n'
+				'^This ^is ^an [^automated ^bot](http://github.com/WinneonSword/LFB)^. ^For ^reporting ^**problems**, ^contact ^/u/WinneonSword.\n\n'
+				'^The ^parent ^commenter ^may [^remove](http://www.np.reddit.com/message/compose?to=LinkFixerBotSnr&subject=Comment%20Deletion&message=%2Bdelete+' + replied.id + ') ^this ^comment ^if ^they ^wish.'
+			)
+			
+			replied.edit(reply)
 			
 			print('> Comment posted! Fixed links: %s' % fixed)
 			
